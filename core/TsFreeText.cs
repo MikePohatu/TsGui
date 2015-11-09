d352a7e9f8aa20fa12eed484241311664233c7b3
Mike Pohatu <mikepohatu@gmail.com>
2015-11-09 21:50:40 +1300

    no gui option finished yet. started tsfreetext and basics

--- core/TsFreeText.cs
+++ core/TsFreeText.cs
@@ -1,17 +1,49 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace core
 {
     public class TsFreeText: IGuiOption
     {
-        private TsVariable tsvar;
+        //private TsVariable tsvar;
         private string name;
         private string value;
 
         public TsVariable TsVariable
         {
-            get { return tsvar; }
+            get { return new TsVariable(this.name,this.value); }
             //set { this.tsvar = value; }
         }
+
+        public bool LoadXml(XElement pXml)
+        {
+            XElement x;
+            bool update = false;
+
+            x = pXml.Element("DefaultName");
+            if (x != null)
+            { this.name = pXml.Element("DefaultName").Value; }
+            else
+            { update = true; }
+
+            x = pXml.Element("MaxInterval");
+            if (x != null)
+            { this.value = pXml.Element("DefaultValue").Value; }
+            else
+            { update = true; }
+
+            return update;
+        }
+
+        //public XElement GetXml()
+        //{
+        //    XElement x = new XElement("SystemSettings",
+        //        new XElement("DefaultInterval", this.DefaultInterval),
+        //        new XElement("MinInterval", this.MinInterval),
+        //        new XElement("MaxInterval", this.MaxInterval));
+
+        //    return x;
+        //    //this.Interval = XmlConvert.ToInt32(pXml.Element("Interval").Value);
+        //}
     }
 }
--- core/TsVariable.cs
+++ core/TsVariable.cs
@@ -4,7 +4,21 @@ namespace core
 {
     public class TsVariable
     {
-        public string Value;
-        public string Name;
+        public string Value { get; set; }
+        public string Name 
+        { 
+            get; 
+            set
+            {
+                if (value == null) { throw new InvalidOperationException("TsVariable name cannot be null"); }
+                else { this.Name = value; }
+            }
+        }
+
+        public TsVariable (string pName, string pValue)
+        {
+            this.Name = pName;
+            this.Value = pValue;
+        }
     }
 }
