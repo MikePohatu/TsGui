b9ba027f967d1b354d3afc693bc3eb301fa56035
Mike Pohatu <mikepohatu@gmail.com>
2015-11-28 16:40:01 +1300

    grid partly working. row setup needs work in column

--- gui-lib/Column.cs
+++ gui-lib/Column.cs
@@ -9,7 +9,7 @@ namespace gui_lib
     public class Column
     {
         private List<IGuiOption> options = new List<IGuiOption>();
-        private StackPanel columnpanel;
+        private Grid columnpanel;
 
         public int Index { get; set; }
         public List<IGuiOption> Options { get { return this.options; } }
@@ -55,21 +55,38 @@ namespace gui_lib
 
         public void Build()
         {
-            StackPanel labelpanel = new StackPanel { Orientation = Orientation.Vertical };
-            StackPanel controlspanel = new StackPanel { Orientation = Orientation.Vertical };
-            this.columnpanel = new StackPanel { Orientation = Orientation.Horizontal };
+            //StackPanel labelpanel = new StackPanel { Orientation = Orientation.Vertical };
+            //StackPanel controlspanel = new StackPanel { Orientation = Orientation.Vertical };
+            //this.columnpanel = new StackPanel { Orientation = Orientation.Horizontal };
 
             Grid colGrid = new Grid();
-            colGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            ColumnDefinition coldefControls = new ColumnDefinition();
+            ColumnDefinition coldefLabels = new ColumnDefinition();
+
+            //coldefControls.
+            colGrid.ColumnDefinitions.Add(coldefLabels);
+            colGrid.ColumnDefinitions.Add(coldefControls);
+            
+            //colGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = "1*" });
             foreach (IGuiOption option in this.options)
             {
+                RowDefinition coldefRow = new RowDefinition();
+                colGrid.RowDefinitions.Add(coldefRow);
+                //coldefRow.Height = new gridl 20;
                 //colGrid.Children.Add
                 //labelpanel.Children.Add(option.Label);
                 //controlspanel.Children.Add(option.Control);
+                coldefRow.
+                Grid.SetColumn(option.Label, 0);
+                Grid.SetColumn(option.Control, 1);
+
+                colGrid.Children.Add(option.Label);
+                colGrid.Children.Add(option.Control);
             }
 
-            this.columnpanel.Children.Add(labelpanel);
-            this.columnpanel.Children.Add(controlspanel);
+            this.columnpanel = colGrid;
+            //this.columnpanel.Children.Add(labelpanel);
+            //this.columnpanel.Children.Add(controlspanel);
         }
     }
 }
