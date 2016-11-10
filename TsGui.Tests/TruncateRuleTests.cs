4cac150ff2534d054ed76ffbb3886016fd935ae8
Mike Pohatu <mikepohatu@gmail.com>
2016-11-10 19:57:39 +1300

    0.9.6.1 feature update. truncate rule for use with query results and tests added

---
+++ TsGui.Tests/TruncateRuleTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using TsGui.Queries;
+using System.Xml.Linq;
+
+namespace TsGui.Tests
+{
+    [TestFixture]
+    public class TruncateRuleTests
+    {
+        [Test]
+        [TestCase("2016","2", ExpectedResult = "16")]
+        [TestCase("asdfliiiKKasdf", "5", ExpectedResult = "Kasdf")]
+        [TestCase("asdf", "5", ExpectedResult = "asdf")]
+        public string KeepFromEndTest(string Input, string Number)
+        {
+            XElement x = new XElement("Truncate");
+            x.Add(new XAttribute("Type", "KeepFromEnd"));
+            x.Value = Number;
+
+            TruncateRule rule = new TruncateRule(x);
+            return rule.Process(Input);
+        }
+
+        [Test]
+        [TestCase("2016", "2", ExpectedResult = "20")]
+        [TestCase("asdfliiiKKasdf", "5", ExpectedResult = "asdfl")]
+        [TestCase("asdf", "5", ExpectedResult = "asdf")]
+        public string KeepFromStartTest(string Input, string Number)
+        {
+            XElement x = new XElement("Truncate");
+            x.Add(new XAttribute("Type", "KeepFromStart"));
+            x.Value = Number;
+
+            TruncateRule rule = new TruncateRule(x);
+            return rule.Process(Input);
+        }
+
+        [Test]
+        [TestCase("2016", "2", ExpectedResult = "16")]
+        [TestCase("asdfliiiKKasdf", "5", ExpectedResult = "iiiKKasdf")]
+        [TestCase("asdf", "5", ExpectedResult = "")]
+        public string RemoveFromStartTest(string Input, string Number)
+        {
+            XElement x = new XElement("Truncate");
+            x.Add(new XAttribute("Type", "RemoveFromStart"));
+            x.Value = Number;
+
+            TruncateRule rule = new TruncateRule(x);
+            return rule.Process(Input);
+        }
+
+        [Test]
+        [TestCase("2016", "2", ExpectedResult = "20")]
+        [TestCase("asdfliiiKKasdf", "5", ExpectedResult = "asdfliiiK")]
+        [TestCase("asdf", "5", ExpectedResult = "")]
+        public string RemoveFromEndTest(string Input, string Number)
+        {
+            XElement x = new XElement("Truncate");
+            x.Add(new XAttribute("Type", "RemoveFromEnd"));
+            x.Value = Number;
+
+            TruncateRule rule = new TruncateRule(x);
+            return rule.Process(Input);
+        }
+    }
+}
