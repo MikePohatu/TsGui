0c53e28c6900f0f1e503dcb72396bfc7bb575b57
Mike Pohatu <mikepohatu@gmail.com>
2016-11-26 22:21:27 +1300

    fixed bug with xml schema not matching the required xml for getvaluefromlist.

--- TsGui/Blind/BlindOption.cs
+++ TsGui/Blind/BlindOption.cs
@@ -24,6 +24,7 @@ namespace TsGui.Blind
     {
         private string _inactivevalue = "TSGUI_INACTIVE";
         private string _value = string.Empty;
+        private bool _usecurrent = false;
 
         //properties
         public string VariableName { get; set; }
@@ -54,29 +55,25 @@ namespace TsGui.Blind
         {
             base.LoadXml(InputXml);
             this.VariableName = XmlHandler.GetStringFromXElement(InputXml, "Variable", this.VariableName);
-            this._value = XmlHandler.GetStringFromXElement(InputXml, "Value", this._value);
 
             this.VariableName = XmlHandler.GetStringFromXAttribute(InputXml, "Variable", this.VariableName);
             this._value = XmlHandler.GetStringFromXAttribute(InputXml, "Value", this._value);
+
             this.InactiveValue = XmlHandler.GetStringFromXElement(InputXml, "InactiveValue", this.InactiveValue);
+            this._usecurrent = XmlHandler.GetBoolFromXAttribute(InputXml, "UseCurrent", this._usecurrent);
 
             XElement x;
-            XAttribute xa;
 
-            x = InputXml.Element("Value");
+            x = InputXml.Element("SetValue");
             if (x != null)
             {
-                xa = x.Attribute("UseCurrent");
-                if (xa != null)
+                if (this._usecurrent == true)
                 {
-                    //default behaviour is to check if the ts variable is already set. If it is, set that as the default i.e. add a query for 
+                    //default behaviour is to NOT check if the ts variable is already set. If it is, set that as the default i.e. add a query for 
                     //an environment variable to the start of the query list. 
-                    if (xa.Value.ToUpper() == "FALSE")
-                    {
-                        XElement xcurrentquery = new XElement("Query", new XElement("Variable", this.VariableName));
-                        xcurrentquery.Add(new XAttribute("Type", "EnvironmentVariable"));
-                        x.AddFirst(xcurrentquery);
-                    }
+                    XElement xcurrentquery = new XElement("Query", new XElement("Variable", this.VariableName));
+                    xcurrentquery.Add(new XAttribute("Type", "EnvironmentVariable"));
+                    x.AddFirst(xcurrentquery);
                 }
 
                 this._value = this._controller.GetValueFromList(x);
