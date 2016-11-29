8ec4a44a101f103ff7c54cef4e4d54f5eead44b6
Mike Pohatu <mikepohatu@gmail.com>
2016-11-29 20:11:40 +1300

    added arrow to tooltip border

--- TsGui/Validation/ValidationToolTipHandler.cs
+++ TsGui/Validation/ValidationToolTipHandler.cs
@@ -45,10 +45,9 @@ namespace TsGui.Validation
             this._popup = new Popup();
             this._popup.AllowsTransparency = true;
             this._popup.Child = this._validationerrortooltip;
-            //this._validationerrortooltip.
             this._popup.PlacementTarget = this._guioption.UserControl;
-            this._popup.Placement = PlacementMode.Right; 
-            //this._popup.HorizontalOffset = 5;
+            if (SystemParameters.MenuDropAlignment == true) { this._popup.Placement = PlacementMode.Left; }
+            else { this._popup.Placement = PlacementMode.Right; }
         }
 
         public void Clear()
