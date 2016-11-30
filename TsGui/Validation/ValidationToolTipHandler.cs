a5a69a4e0c91146686b4ec17ae69dd9fe2280a22
Mike Pohatu <mikepohatu@gmail.com>
2016-12-01 08:10:59 +1300

    fixed placement not setting properly with labelonright set to true

--- TsGui/Validation/ValidationToolTipHandler.cs
+++ TsGui/Validation/ValidationToolTipHandler.cs
@@ -50,11 +50,7 @@ namespace TsGui.Validation
             this._popup = new Popup();
             this._popup.AllowsTransparency = true;
             this._popup.Child = this._validationerrortooltip;
-            this._popup.PlacementTarget = this._guioption.Control;
-
-            //this is to handle WPF quirks with touch devices
-            if (SystemParameters.MenuDropAlignment == false) { this._popup.Placement = PlacementMode.Right; }
-            else { this._popup.Placement = PlacementMode.Left; }
+            this._popup.PlacementTarget = this._guioption.Control; 
         }
 
         public void Clear()
@@ -68,6 +64,7 @@ namespace TsGui.Validation
 
         public void Show()
         {
+            this.SetPlacement();
             this._controller.WindowMouseUp += OnWindowMouseUp;
             this._popup.IsOpen = true; 
             this._guioption.ControlFormatting.BorderBrush.Color = Colors.Red;
@@ -83,9 +80,9 @@ namespace TsGui.Validation
 
         private void Refresh()
         {
-            this.ResetPlacement();
+            this.ResetLocation();
 
-            if (this.HasHitRightScreenEdge() == this._guioption.LabelOnRight)
+            if (this.HasHitRightScreenEdge() == false)
             {
                 this._validationerrortooltip.LeftArrow.Visibility = Visibility.Visible;
                 this._validationerrortooltip.RightArrow.Visibility = Visibility.Hidden;
@@ -109,10 +106,25 @@ namespace TsGui.Validation
             else { return true; }
         }
 
-        private void ResetPlacement()
+        private void ResetLocation()
         {
             this._popup.IsOpen = false;
             this._popup.IsOpen = true;
+        }
+
+        private void SetPlacement()
+        {
+            //this is to handle WPF quirks with touch devices
+            if (this._guioption.LabelOnRight == false)
+            {
+                if (SystemParameters.MenuDropAlignment == false) { this._popup.Placement = PlacementMode.Right; }
+                else { this._popup.Placement = PlacementMode.Left; }
+            }
+            else
+            {
+                if (SystemParameters.MenuDropAlignment == false) { this._popup.Placement = PlacementMode.Left; }
+                else { this._popup.Placement = PlacementMode.Right; }
+            }
         }
     }
 }
