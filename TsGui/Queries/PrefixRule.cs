06bc9449c62df0f7e0bfc4981908fbf5cc8f8df2
Mike Pohatu <mikepohatu@gmail.com>
2017-04-09 20:37:21 +1200

    updated description

--- TsGui/Queries/PrefixRule.cs
+++ TsGui/Queries/PrefixRule.cs
@@ -13,7 +13,7 @@
 //    with this program; if not, write to the Free Software Foundation, Inc.,
 //    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-// AppendRule.cs - used to add a string to the end of a string result
+// Prefix.cs - used to add a string to the start of a string result
 
 using System.Xml.Linq;
 
