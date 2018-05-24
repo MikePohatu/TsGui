c1a404e0cc44b1e9e8465e62e158006bd3e89aaf
Mike Pohatu <mikepohatu@gmail.com>
2018-05-25 09:19:45 +1200

    Added tolower and toupper property rules

--- TsGui/Properties/AssemblyInfo.cs
+++ TsGui/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("20Road")]
 [assembly: AssemblyProduct("Task Sequence Gui")]
-[assembly: AssemblyCopyright("Copyright © Mike Pohatu 2017")]
+[assembly: AssemblyCopyright("Copyright © Mike Pohatu 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6.1")]
-[assembly: AssemblyFileVersion("1.0.6.1")]
+[assembly: AssemblyVersion("1.0.6.2")]
+[assembly: AssemblyFileVersion("1.0.6.2")]
 [assembly: NeutralResourcesLanguage("")]
 
