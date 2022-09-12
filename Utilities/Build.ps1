1b2f331b9a33e3cbd859ece635ffd4a872ca816b
Mike Pohatu <mike@20road.com>
2022-09-12 22:57:20 +1200

    build | updated build script for new visual studio and added some logic to not need to build signed code e.g. to run a build for testing

--- Utilities/Build.ps1
+++ Utilities/Build.ps1
@@ -1,4 +1,4 @@
-﻿Function SignAssembliesInPath {
+Function SignAssembliesInPath {
     Param(
         [Parameter(Mandatory=$true)][string]$PackagePath,
         [string]$SignPath='C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64',
@@ -88,7 +88,7 @@ Function pause ()
 #run builds
 $version = '1.5.1.2'
 $repoRoot = 'C:\Source\repos\TsGui'
-$devenv = 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe'
+$devenv = 'C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe'
 $dotnet = 'dotnet.exe'
 $cmd = "cmd"
 $productName = "TsGui"
@@ -117,40 +117,54 @@ if (Test-Path $ProductReleasePath) {
 md $ProductReleasePath -Force
 md "$($ProductReleasePath)\Config_Examples" -Force
 md "$($ProductReleasePath)\images" -Force
+md "$($ProductReleasePath)\scripts" -Force
 
 Copy-Item -Path "$($repoRoot)\Config_Examples\*" -Destination "$($ProductReleasePath)\Config_Examples" -Recurse -Force
 Copy-Item -Path "$($repoRoot)\images\*" -Destination "$($ProductReleasePath)\images" -Recurse -Force
+Copy-Item -Path "$($ProjectReleasePath)\scripts\*" -Destination "$($ProductReleasePath)\scripts" -Recurse -Force
 Copy-Item -Path "$($repoRoot)\Release notes.txt" -Destination "$($ProductReleasePath)" -Force
 Copy-Item -Path "$($repoRoot)\Documentation and Source.url" -Destination "$($ProductReleasePath)" -Force
 Copy-Item -Path "$($repoRoot)\Config.xml" -Destination "$($ProductReleasePath)" -Force
 Copy-Item -Path "$($repoRoot)\Config_demo.xml" -Destination "$($ProductReleasePath)" -Force
-Copy-Item -Path "$($ProjectReleasePath)\NLog.config" -Destination "$($ProductReleasePath)" -Force
-Copy-Item -Path "$($ProjectReleasePath)\NLog.dll" -Destination "$($ProductReleasePath)" -Force
-Copy-Item -Path "$($ProjectReleasePath)\Messaging.dll" -Destination "$($ProductReleasePath)" -Force
-Copy-Item -Path "$($ProjectReleasePath)\TsGui.exe" -Destination "$($ProductReleasePath)" -Force
+
+
+$appFiles = @("TsGui.exe", 
+    "Core.dll", 
+    "Messaging.dll", 
+    "Microsoft.Management.Infrastructure.dll", 
+    "Newtonsoft.Json.dll", 
+    "NLog.dll", "NLog.config",
+    "System.Management.Automation.dll", 
+    "WindowsHelpers.dll")
+
+$appFiles | ForEach-Object {
+    Copy-Item -Path "$($ProjectReleasePath)\$_" -Destination "$($ProductReleasePath)" -Force
+}
 
 #Create the unsigned package zip
 PackageFolder -PackageFolder $ProductReleasePath -DestinationPath "$($ReleaseRootPath)\$($productName)_$($version).zip"
 
 
+$confirm = Read-Host -Prompt "Do you want to sign binaries?[y/n]"
+if ( $confirm -match "[yY]" ) { 
+    Write-Host "Signing assemblies"
+    SignAssembliesInPath -PackagePath $ProductReleasePath -Description $productName
 
-Pause -message "Click OK to begin signing binaries"
-Write-Host "Signing assemblies"
-SignAssembliesInPath -PackagePath $ProductReleasePath -Description $productName
+    $appstatus = VerifyAssembliesInPath -PackagePath $ProductReleasePath
 
-$appstatus = VerifyAssembliesInPath -PackagePath $ProductReleasePath
+    $allstatus = @()
+    $allstatus += $appstatus
 
-$allstatus = @()
-$allstatus += $appstatus
+    $errorfound = $false
 
-$errorfound = $false
-
-$allstatus | ForEach-Object { 
-    if ($_.Status -ne "Valid") { 
-        $errorfound = $true
-        break
+    $allstatus | ForEach-Object { 
+        if ($_.Status -ne "Valid") { 
+            $errorfound = $true
+            break
+        }
     }
+
+    #Create the signed package zip
+    PackageFolder -PackageFolder $ProductReleasePath -DestinationPath "$($ReleaseRootPath)\$($productName)_$($version)_signed.zip"
 }
 
-#Create the signed package zip
-PackageFolder -PackageFolder $ProductReleasePath -DestinationPath "$($ReleaseRootPath)\$($productName)_$($version)_signed.zip"
