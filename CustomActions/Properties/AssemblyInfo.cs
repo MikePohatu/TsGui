961356e8b527b3585c4a6c9606526157668cc0c7
Mike Pohatu <mike@20road.com>
2022-09-14 14:31:43 +1200

    release | v2.0.0.0

--- CustomActions/Properties/AssemblyInfo.cs
+++ CustomActions/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("20Road Limited")]
-[assembly: AssemblyProduct("DevChecker")]
-[assembly: AssemblyCopyright("Copyright © 20Road Limited 2021")]
+[assembly: AssemblyProduct("TsGui")]
+[assembly: AssemblyCopyright("Copyright © 20Road Limited 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -50,5 +50,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.5.0")]
-[assembly: AssemblyFileVersion("0.9.5.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
