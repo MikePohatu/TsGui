5c9e413bc5a7f50696ff973728de23b209609503
Mike Pohatu <mike@20road.com>
2023-05-03 12:29:42 +1200

    feature | added borders

---
+++ TsGui/View/Layout/Border.cs
@@ -0,0 +1,43 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Xml.Linq;
+using TsGui.View.GuiOptions;
+
+namespace TsGui.View.Layout
+{
+    public class Border : ViewModelBase
+    {
+        private SolidColorBrush _brush;
+        public SolidColorBrush Brush
+        {
+            get { return this._brush; }
+            set { this._brush = value; this.OnPropertyChanged(this, "Brush"); }
+        }
+
+        private Thickness _thickness = new Thickness(0);
+        public Thickness Thickness
+        {
+            get { return this._thickness; }
+            set
+            {
+                this._thickness = value;
+                this.OnPropertyChanged(this, "Thickness");
+            }
+        }
+
+        public void LoadXml(XElement InputXml)
+        {
+            if (InputXml != null)
+            {
+                this.Brush = XmlHandler.GetSolidColorBrushFromXml(InputXml, "Color", this._brush);
+                this.Thickness = XmlHandler.GetThicknessFromXml(InputXml, "Thickness", this._thickness);
+            }
+        }
+    }
+}
